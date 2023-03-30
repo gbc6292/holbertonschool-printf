314692c3e76270f69f186fc587a6fbb7ed1dc8f8
gbc6292 <6292@holbertonstudents.com>
2023-03-30 16:47:28 +0000

    First test printf

---
+++ pr_int.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+int pr_int(int num)
+{
+        int count = 0;
+        int digits[10];
+        int j = 0;
+
+        if (num < 0)
+        {
+                _putchar('-');
+                count++;
+                num = -num;
+        }
+        if (num == 0)
+        {
+                _putchar('0');
+                count++;
+        }
+        else
+        {
+                while (num > 0)
+                {
+                        digits[j] = num % 10;
+                        num /= 10;
+                        j++;
+                }
+                while (j > 0)
+                {
+                        j--;
+                        _putchar (digits[j] + '0');
+                        count++;
+                }
+        }
+        return (num);
+}
