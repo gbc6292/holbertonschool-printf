0e3a579c201502db7a3d36b629da139c6a67323e
gbc6292 <6292@holbertonstudents.com>
2023-03-31 03:20:12 +0000

    test 7

--- pr_func.c
+++ pr_func.c
@@ -1,10 +1,22 @@
 #include "main.h"
+/**
+ * print_char - Function that prints char
+ * @a: Variable that contain the character to be printed.
+ * Return: 1
+ *
+ */
 int print_char(char a)
 {
 	_putchar(a);
 	return (1);
 }
 
+/**
+ * print_string - Function that print a string.
+ * @b: Pointer that contains the string to be printed.
+ * Return: The string
+ *
+ */
 int print_string(char *b)
 {
 	int in;
