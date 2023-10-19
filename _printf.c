25c0126968e5818b35d6fba777726f83ab7c78fa
Giovanni Barreto <124797071+gbc6292@users.noreply.github.com>
2023-10-18 21:54:17 -0400

    Documenting properly _printf code

--- _printf.c
+++ _printf.c
@@ -1,58 +1,67 @@
 #include <stdarg.h>
 #include "main.h"
+
 /**
- * _printf - Function that print, strings, characters and integers.
- * @format: Format of the argument to be printed.
- * Return: A printed argument.
- *
+ * _printf - Custom printf function that prints strings, characters, and integers.
+ * @format: A format string containing format specifiers.
+ * @...: Variable number of arguments corresponding to the format specifiers.
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	int idx = 0, x = 0;
-	va_list list;
+	int idx = 0; // Index to traverse the format string
+	int x = 0;   // Counter for the number of characters printed
+	va_list list; // Variable argument list
 
-	va_start(list, format);
+	va_start(list, format); // Initialize the variable argument list
+
+	// Check for empty format string
 	if (format[idx] == '\0')
 		return (0);
+
+	// Check if the variable argument list is empty
 	if (list == '\0')
 		return (0);
+
 	while (format[idx] != '\0')
 	{
 		if (format[idx] == '%')
 		{
+			// Handle format specifiers
 			switch (format[idx + 1])
 			{
 				case 'c':
 					{
-						x += print_char(va_arg(list, int));
+						x += print_char(va_arg(list, int)); // Print a character
 						break;
 					}
 				case 's':
 					{
-						x += print_string(va_arg(list, char *));
+						x += print_string(va_arg(list, char *)); // Print a string
 						break;
 					}
 				case '%':
 					{
-						x += print_char('%');
+						x += print_char('%'); // Print a literal '%'
 						break;
 					}
 				case 'd':
 				case 'i':
 					{
-						x += pr_int(va_arg(list, int));
+						x += pr_int(va_arg(list, int)); // Print an integer
 						break;
 					}
 			}
-				idx++;
+			idx++; // Move to the next character in the format string
 		}
-
 		else
 		{
+			// Print regular characters
 			x += print_char(format[idx]);
 		}
-		idx++;
+		idx++; // Move to the next character in the format string
 	}
-	va_end(list);
-	return (x);
+
+	va_end(list); // Clean up the variable argument list
+	return (x); // Return the total number of characters printed
 }
